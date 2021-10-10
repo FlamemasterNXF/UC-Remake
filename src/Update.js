1c203aab1c33f5ab9d06d8c95a6e8acd10a0b5a6
FlamemasterNXF <flamemasternxf@gmail.com>
2021-10-09 21:34:57 -0400

    Text fix

--- src/Update.js
+++ src/Update.js
@@ -5,6 +5,6 @@ function updateHTML(){
     document.getElementById("derivI").innerHTML = `Derivative I: [Increase]<br>Cost: ${format(data.derivs[0].c)}<br>Creates 1 Oddity every second.`
     for (let i=1; i<data.derivs.length; i++){
         document.getElementById(`deriv${derivNames[i]}`).innerHTML =
-            `Derivative ${derivNames[i]}: [${derivTitles[i]}]<br>Cost: ${format(data.derivs[i].c)}<br>Creates 1 D.${derivNames[i]} every second.`
+            `Derivative ${derivNames[i]}: [${derivTitles[i]}]<br>Cost: ${format(data.derivs[i].c)}<br>Creates 1 D.${derivNames[i-1]} every second.`
     }
 }
