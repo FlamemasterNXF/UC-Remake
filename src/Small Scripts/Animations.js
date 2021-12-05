685695191f7607567314df63a0bb82a0afd6ed09
FlamemasterNXF <flamemasternxf@gmail.com>
2021-12-05 11:55:36 -0500

    MOAR ANIMATION

--- src/Small Scripts/Animations.js
+++ src/Small Scripts/Animations.js
@@ -54,11 +54,11 @@ function reloadWindow() {
     ctx.scale(smallSide/1250,smallSide/1250);
 }
 
-setInterval(mainLoop,20);
+setInterval(animationsLoop,20);
 
 let timer = 0;
 
-function mainLoop() {
+function animationsLoop() {
     let win = window;
     let doc = document;
     let docElem = doc.documentElement;
@@ -94,30 +94,3 @@ function mainLoop() {
     ctx.fillStyle = "black";
     drawRect(-1,-1,1,1);
 }
-(function() {
-    document.onmousemove = handleMouseMove;
-    function handleMouseMove(event) {
-        let eventDoc, doc, body;
-
-        event = event || window.event; // IE-ism
-
-        // If pageX/Y aren't available and clientX/Y are,
-        // calculate pageX/Y - logic taken from jQuery.
-        // (This is to support old IE)
-        if (event.pageX == null && event.clientX != null) {
-            eventDoc = (event.target && event.target.ownerDocument) || document;
-            doc = eventDoc.documentElement;
-            body = eventDoc.body;
-
-            event.pageX = event.clientX +
-                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-                (doc && doc.clientLeft || body && body.clientLeft || 0);
-            event.pageY = event.clientY +
-                (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-                (doc && doc.clientTop  || body && body.clientTop  || 0 );
-        }
-
-        mouseX = event.pageX-(canX/2);
-        mouseY = event.pageY-(canY/2);
-    }
-})();
