723a4c9db5f0f7e1a3c5e3e4608ae9eeec56d2f1
FlamemasterNXF <flamemasternxf@gmail.com>
2022-08-03 23:52:55 -0400

    fix: Cycle buymax

--- src/Circles.js
+++ src/Circles.js
@@ -34,60 +34,51 @@ const CYCLES = {
         effect(){ if (this.active() && data.cycleLevels[0].gte(1)){ return (c().log2().sqrt()).plus(data.cycleLevels[0]).sub(this.nerf()).clampMin(1) } else { return D(1) } },
         cost(){return D(5e4).times(data.cycleLevels[0].plus(data.cycleLevels[0].div(10).plus(1)))},
         desc(){return `Current Multiplier: ${format(this.effect())}x\nCurrent Nerf: -${format(this.nerf())}`},
-        level: D(0)
     },
     2: {
         active(){ return data.inLost },
         effect(){ if (this.active() && data.cycleLevels[1].gte(1)){ return (c().log2().sqrt()).plus((data.cycleLevels[1])).plus(data.derivs[0].b).clampMin(1) } else { return D(1) } },
         cost(){return D(5e4).times(data.cycleLevels[1].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x`},
-        level: D(0)
     },
     3: {
         effect(){ if(data.cycleLevels[2].gte(1)){ return (c().log2().sqrt()).plus(data.particles[2].sqrt().log10()).sqrt().plus((data.cycleLevels[2])).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[2].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Boost: +${format(this.effect())}`},
-        level: D(0)
     },
     4: {
         effect(){ if(data.cycleLevels[3].gte(1)){ return (c().log2().sqrt()).plus(data.particles[0].sqrt()).sqrt().log2().plus((data.cycleLevels[3])) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[3].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x`},
-        level: D(0)
     },
     5: {
         nerf(){ if(data.cycleLevels[4].gte(1)){ return (data.cycleLevels[4].div(10).plus(1)).div(theoryEffects[16]) } else { return D(1) } },
         effect(){ if(data.cycleLevels[4].gte(1)){ return (c().sqrt()).plus((data.cycleLevels[4])).div(2).div(CYCLES[6].nerf()).div(BREAKPOINTS[2].nerf()).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[4].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x\nCurrent Nerf: /${format(CYCLES[6].nerf())}`},
-        level: D(0)
     },
     6: {
         nerf(){ if(data.cycleLevels[5].gte(1)){ return (data.cycleLevels[5].div(10).plus(1)).div(theoryEffects[16]) } else { return D(1) } },
         effect(){ if(data.cycleLevels[5].gte(1)){ return (c().sqrt()).plus((data.cycleLevels[5])).div(2).div(CYCLES[5].nerf()).div(BREAKPOINTS[2].nerf()).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[5].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x\nCurrent Nerf: /${format(CYCLES[5].nerf())}`},
-        level: D(0)
     },
     7: {
         effect(){ if(data.cycleLevels[6].gte(1)){ return (c().log2().sqrt()).plus(data.lostCycleLevels[1].sqrt()).plus((data.cycleLevels[6])).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[6].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x`},
-        level: D(0)
     },
     8: {
         nerf(){ if(data.derivs[3].b.gte(1)){ return data.cycleLevels[7].gte(1)?data.derivs[3].b:D(1) } else { return D(1) } },
         effect(){ if(data.cycleLevels[7].gte(1)){ return ((c().log2().sqrt()).plus(data.derivs[0].b)).sqrt().plus((data.cycleLevels[7])).sub(this.nerf()).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[7].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x\nCurrent Nerf: -${format(this.nerf())}`},
-        level: D(0)
     },
     9: {
         nerf(){ if(data.derivs[1].b.gte(1)){ return data.cycleLevels[8].gte(1)?data.lostCycleLevels[1].div(2):D(1) } else { return D(1) } },
         effect(){ if(data.cycleLevels[8].gte(1)){ return ((c().log2().sqrt()).plus(data.particles[2].sqrt())).sqrt().div(10).plus((data.cycleLevels[8])).sub(this.nerf()).clampMin(1) } else { return D(1) } },
         cost(){return D(1e5).times(data.cycleLevels[8].plus(data.cycleLevels[0].div(10).plus(1))) },
         desc(){return `Current Multiplier: ${format(this.effect())}x\nCurrent Nerf: -${format(this.nerf())}`},
-        level: D(0)
     },
 }
 const BREAKPOINTS = {
@@ -142,7 +133,7 @@ const SECRETS = {
 function buyCycle(i){
     if(data.particles[1].gte(CYCLES[i].cost())){
         data.particles[1] = data.particles[1].sub(CYCLES[i].cost())
-        CYCLES[i].level = CYCLES[i].level.plus(1); data.cycleLevels[i-1] = data.cycleLevels[i-1].plus(1)
+        data.cycleLevels[i-1] = data.cycleLevels[i-1].plus(1)
     }
 }
 function toggleBreakPoint(i){
@@ -184,29 +175,27 @@ function changeCirclesTab(i){
     if(i!=='secrets') DOM('secretsContainer').style.display = 'none'
 }
 function buyMaxCycles(){
-  for(let i=2;i<=10;i++){
+  for(let i=1;i<=9;i++){
     let dp = data.particles[1]
     if(dp.lte(0))break
-    let startLevel = CYCLES[i==10?1:i].level // buy cycle 1 last
-    if(i==10){
+    let startLevel = data.cycleLevels[i===9?1:i] // buy cycle 1 last
+    if(i===10){
       let spent = startLevel.mul(startLevel).mul(0.55).add(startLevel.mul(1.55)).mul(5e4)
       let maxLevels = dp.div(5e4).add(spent.div(5e4)).mul(880).add(961).sqrt().div(22).sub(31/22).floor().clampMin(startLevel)
-      CYCLES[1].level = maxLevels
       data.cycleLevels[0] = maxLevels
       let totalCost = maxLevels.mul(maxLevels).mul(0.55).add(maxLevels.mul(1.55)).mul(5e4).sub(spent)
       data.particles[1]=data.particles[1].sub(totalCost)
     } else {
-      let a = CYCLES[1].level.div(10).add(1)
-      let spent = startLevel.mul(startLevel.add(1)).div(2).add(a.mul(startLevel)).mul(i==2?5e4:1e5)
-      let maxLevels = dp.div(11-i).div(i==2?5e4:1e5).add(spent.div(i==2?5e4:1e5)).mul(8).add(a.mul(2).add(1).pow(2)).sqrt().div(2).sub(a.add(0.5)).clampMin(startLevel).floor()
+      let a = data.cycleLevels[0].div(10).add(1)
+      let spent = startLevel.mul(startLevel.add(1)).div(2).add(a.mul(startLevel)).mul(i===1?5e4:1e5)
+      let maxLevels = dp.div(11-i).div(i===1?5e4:1e5).add(spent.div(i===1?5e4:1e5)).mul(8).add(a.mul(2).add(1).pow(2)).sqrt().div(2).sub(a.add(0.5)).clampMin(startLevel).floor()
       // the 11-i is there to distribute dp evenly
-      CYCLES[i].level = maxLevels
-      data.cycleLevels[i-1] = maxLevels
-      let totalCost = maxLevels.mul(maxLevels.add(1)).div(2).add(a.mul(maxLevels)).mul(i==2?5e4:1e5).sub(spent)
+      data.cycleLevels[i] = maxLevels
+      let totalCost = maxLevels.mul(maxLevels.add(1)).div(2).add(a.mul(maxLevels)).mul(i===1?5e4:1e5).sub(spent)
       data.particles[1]=data.particles[1].sub(totalCost)
     }
   }
-  
+
     /*let w=0
     while(w < 11){
         for (let i=1;i<10;i++){
