a444eb222d4e063cb41c19aef4317c0403e34c05
FlamemasterNXF <flamemasternxf@gmail.com>
2022-08-09 14:53:48 -0400

    minor: Move File Locations

--- src/Internal/Minor.js
+++ src/Internal/Minor.js
@@ -11,3 +11,18 @@ const DOM = (id) => {
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min
 }
+
+/*MODE OPTIONS
+OF: ON/OFF
+OFL: On/Off
+ED: ENABLED/DISABLED
+EDL: Enabled/Disabled
+UL: Unlocked/Locked
+*/
+function boolToReadable (bool, mode='OF'){
+    if(mode==='OF') return bool?'ON':'OFF'
+    if(mode==='OFL') return bool?'On':'Off'
+    if(mode==='ED') return bool?'ENABLED':'DISABLED'
+    if(mode==='EDL') return bool?'Enabled':'Disabled'
+    if(mode==='UL') return bool?'Unlocked':'Locked'
+}
