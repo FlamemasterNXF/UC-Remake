0ae555b592f5852247fdb2ee46d2e6f7207f16dd
FlamemasterNXF <flamemasternxf@gmail.com>
2022-08-13 21:20:01 -0400

    safety: DOM element access

--- src/Internal/Helpers.js
+++ src/Internal/Helpers.js
@@ -3,6 +3,7 @@ const DOM = (id) => {
     const cachedEl = DOMCache.get(id)
     if (cachedEl) return cachedEl
     const el = document.getElementById(id)
+    if(el===null) throw `Element ${id} is null.`
     DOMCache.set(id, el)
     return el
 }
