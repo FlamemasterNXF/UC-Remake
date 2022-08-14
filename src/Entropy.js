f1072501565538b84912017d88ccc86910cc02ac
FlamemasterNXF <flamemasternxf@gmail.com>
2022-08-13 22:24:59 -0400

    minor: Boost Entropy after e300 Oddities

--- src/Entropy.js
+++ src/Entropy.js
@@ -1,6 +1,8 @@
 const ENTROPY = {
     entropyGain() {
-        if(data.oddities.gt(1e150) && data.inversionInversionControl[1] && data.inversions.gte(3)){ return (data.oddities.log10()).pow(INVERSIONS.deepInversionEffects()[2]).times(INVERSIONS.inversionEffect().log2()) }
+        let cond = data.inversionInversionControl[1] && data.inversions.gte(3)
+        if(data.oddities.gte(1e300) && cond){ return (data.oddities.sqrt()).pow(INVERSIONS.deepInversionEffects()[2]).times(INVERSIONS.inversionEffect().log2()) }
+        if(data.oddities.gt(1e150) && cond ){ return (data.oddities.log10()).pow(INVERSIONS.deepInversionEffects()[2]).times(INVERSIONS.inversionEffect().log2()) }
         if(data.oddities.gt(1e150)){ return (data.oddities.log10()).pow(INVERSIONS.deepInversionEffects()[2]) }
         else{ return D(0) }
     },
